a2fb5554282f6463155f3b41560c37abddd99692
nesterok-d <nesr.daria.nest@gmail.com>
2022-10-10 17:45:39 +0700

    added 10.10.22

--- cypress/e2e/pages/MainPage.js
+++ cypress/e2e/pages/MainPage.js
@@ -18,7 +18,7 @@ class MainPage {
     catalog = 'div.swiper-slide-duplicate'
     
     
-    leftMeтuMobile = 'div[class = "header-mobile__top"] button svg[data-name="IconMenu"]'
+    leftMeтuMobile = 'div#root-header button[title="Меню"]'
     cityListMobile = '#root-header > header > div > div.header-mobile__wrapper.open > nav > div.header-mobile__nav-main > div:nth-child(1) > button'
     cityInHeaderMobile = '#root-header > header > div > div.header-mobile__wrapper.open > nav > div.header-mobile__nav-main > div:nth-child(1) > button > span:nth-child(2)'
     cityListItemMobile = 'div.cities__result ul li a'
